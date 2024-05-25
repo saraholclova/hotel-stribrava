4c6fa615aecdf087427f8228ea5039f1d33aa37c
Sára Hölclová <sara.holclova@gmail.com>
2024-05-25 14:42:05 +0200

    komponenta RoomDetail a fetch

---
+++ src/Components/RoomDetail/RoomDetail.jsx
@@ -0,0 +1,62 @@
+import { useEffect, useState } from 'react';
+
+export const RoomDetail = ({ roomId }) => {
+  const [room, setRoom] = useState(null);
+
+  useEffect(() => {
+    const fetchRoom = async () => {
+      const response = await fetch(`http://localhost:4000/api/rooms/${roomId}`);
+      const json = await response.json();
+      setRoom(json.data);
+    };
+    fetchRoom();
+  }, [roomId]);
+
+  return (
+    <section className="light">
+      <div className="container">
+        <h2>{room.name}</h2>
+        <div className="columns-2">
+          <div className="column">
+            <img src="img/image1.svg" />
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque
+              accusantium, dolor quisquam doloremque quod nobis temporibus
+              ducimus sapiente consectetur distinctio assumenda, nisi suscipit
+              saepe. Vero.
+            </p>
+          </div>
+          <form>
+            <div className="form-fields">
+              <label htmlFor="field1" className="field-label">
+                Field 1:
+              </label>
+              <input id="field1" className="field-input" type="text" />
+
+              <label htmlFor="field2" className="field-label">
+                Field 2:
+              </label>
+              <input id="field2" className="field-input" type="text" />
+
+              <label htmlFor="select" className="field-label">
+                Select:
+              </label>
+              <select id="select" className="field-input">
+                <option>Option 1</option>
+                <option>Option 2</option>
+                <option>Option 3</option>
+                <option>Option 4</option>
+              </select>
+
+              <label htmlFor="check1" className="field-label">
+                Checkbox 1:
+              </label>
+              <input id="check1" className="field-input" type="checkbox" />
+            </div>
+            <button className="wide">Submit</button>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+};
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -2,12 +2,14 @@ import { Header } from '../../components/Header/Header';
 import './style.css';
 import { Footer } from '../../components/Footer/Footer';
 import { RoomList } from '../../components/RoomList/RoomList';
+import { RoomDetail } from '../../components/RoomDetail/RoomDetail';
 
 export const HomePage = () => {
   return (
     <>
       <Header />
       <RoomList />
+      <RoomDetail />
       <Footer />
     </>
   );
