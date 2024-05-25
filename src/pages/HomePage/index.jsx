e30534fdfd05cfec46cdcd59eeaa7c2bd5b13c88
Nikola Lorenc <nikola.skarkova@gmail.com>
2024-05-25 14:14:33 +0200

    uprava pojmenovani slozek

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,6 +1,6 @@
 import { Header } from '../../components/Header/Header';
 import './style.css';
-import { Footer } from '../../Components/Footer/Footer';
+import { Footer } from '../../components/Footer/Footer';
 import { RoomList } from '../../components/RoomList/RoomList';
 
 export const HomePage = () => {
